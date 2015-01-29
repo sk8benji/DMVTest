a970e8f7aa5919f42340c034bb573df266e70650
sk8benji <sk8benji@gmail.com>
2015-01-29 03:39:06 -0800

    aca

--- app/src/main/java/com/delacruz/dmvtest/MainActivity.java
+++ app/src/main/java/com/delacruz/dmvtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +13,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hola56565
+
+        TextView tv = new TextView(this);//apartir de aca es el texto
+        tv.setText("teste");
+        setContentView(tv);
+        //hasta aca
     }
 
 
