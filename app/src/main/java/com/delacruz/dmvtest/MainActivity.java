3df561a14c6018b1c81eb29400245734933fa8ee
sk8benji <sk8benji@gmail.com>
2015-01-31 23:35:24 -0800

    agregados los 3 acciones de los botones
    agregados los 3 acciones de los botones

--- app/src/main/java/com/delacruz/dmvtest/MainActivity.java
+++ app/src/main/java/com/delacruz/dmvtest/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -14,13 +16,39 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = new TextView(this);//esta sirve como ejemplo para aparecer un texto
+       // TextView tv = new TextView(this);//esta sirve como ejemplo para aparecer un texto
         //tv es una variable textview
-        tv.setText("teste");
-        setContentView(tv);
-        findViewById(R.id.textView1);
+        //tv.setText("teste");
+        //setContentView(tv);
+       // findViewById(R.id.textView1);
+        Button face_btn=(Button)findViewById(R.id.facebook_btn); // creo una variable button llamada face_btn relacionada al layout
+        Button login_btn=(Button)findViewById(R.id.online_btn);
+        Button guest_btn=(Button)findViewById(R.id.guest_btn);
 
+        face_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        guest_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+            
+
+        });
+
+        // hasta aca estan creados los 3 botones con el set,on.click listos para hacer la accion
     }
 
 
